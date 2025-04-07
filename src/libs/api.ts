a7f65288670167ee2bd47498ce48b8596a2d8d56
EnergyPatrikHU <energypatrikhu@proton.me>
2025-04-07 13:36:29 +0200

    Refactor API base URL construction for better environment handling

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -1,7 +1,14 @@
 import axios from 'axios';
 
+function constructApiDomain() {
+	const protocol = window.location.protocol;
+	const host = window.location.hostname;
+
+	return `${protocol}//${host}:5543/`;
+}
+
 export const api = axios.create({
-	baseURL: import.meta.env.DEV ? 'https://api.cat-cafe.hu/' : 'http://localhost:3000/',
+	baseURL: import.meta.env.DEV ? 'https://api.cat-cafe.hu/' : constructApiDomain(),
 	headers: {
 		'Content-Type': 'application/json',
 	},
