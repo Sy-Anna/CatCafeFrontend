1928045587b1e012c6818daf92c3c397d406fa84
EnergyPatrikHU <energypatrikhu@proton.me>
2025-04-09 14:01:04 +0200

    Add manual chunking configuration in Vite for optimized build output

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,17 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (id.includes("node_modules")) {
+						return id.split("node_modules/")[1].split("/")[0];
+					}
+				},
+			},
+		},
+	},
 	server: {
 		open: "/Home",
 	},
