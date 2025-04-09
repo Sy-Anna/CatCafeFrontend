42400987de25c77faf41393ade37d58b35ddf942
EnergyPatrikHU <energypatrikhu@proton.me>
2025-04-09 13:05:06 +0200

    Remove redundant id property from home page links and use index as key in map function

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,37 +3,31 @@ import "../css/HomeDark.css";
 
 const pages = [
 	{
-		id: 1,
 		source: "/Blog",
 		imgSource: "/img/homePage/sysAdmin.jpg",
 		title: "Blog",
 	},
 	{
-		id: 1,
 		source: "/Gallery",
 		imgSource: "/img/homePage/img22.jpg",
 		title: "Galéria",
 	},
 	{
-		id: 1,
 		source: "/Booking",
 		imgSource: "/img/homePage/img42.jpg",
 		title: "Asztalfoglalás",
 	},
 	{
-		id: 1,
 		source: "/Webshop",
 		imgSource: "/img/homePage/Catpucino.png",
 		title: "Webshop",
 	},
 	{
-		id: 1,
 		source: "/Contact",
 		imgSource: "/img/homePage/teamImage.jpg",
 		title: "Kapcsolat",
 	},
 	{
-		id: 1,
 		source: "/About",
 		imgSource: "/img/homePage/20240316_004312.png",
 		title: "Rólunk",
@@ -44,8 +38,8 @@ export default function Home() {
 	return (
 		<>
 			<div className='home-card-grid'>
-				{pages.map((page) => (
-					<div className='homeCardContainer' key={page.id}>
+				{pages.map((page, index) => (
+					<div className='homeCardContainer' key={index}>
 						<a href={page.source}>
 							<div className='homeCard'>
 								<img className='homeCardImage' src={page.imgSource} alt='image' />
