2bf39dfa4006672665222207f5486ec57c47028c
EnergyPatrikHU <energypatrikhu@proton.me>
2025-04-11 07:59:21 +0200

    Implement booking functionality; add notification handling and integrate ReservationsApi for creating bookings

--- src/pages/Booking.tsx
+++ src/pages/Booking.tsx
@@ -4,6 +4,8 @@ import "react-calendar/dist/Calendar.css";
 
 import "@assets/css/Calendar.css";
 import "@assets/css/CalendarDark.css";
+import { useNotification } from "@hooks/useNotification";
+import { ReservationsApi } from "@libs/api/reservations";
 
 type ValuePiece = Date | null;
 
@@ -11,10 +13,34 @@ type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 export default function Booking() {
     const [value, onChange] = useState<Value>(new Date());
+    const notification = useNotification();
+
+    const handleBooking = async () => {
+        if (!value || Array.isArray(value)) {
+            notification.add("Érvénytelen dátum!", "error");
+            return;
+        }
+
+        const [error] = await ReservationsApi.create(value);
+        if (error) {
+            if (Array.isArray(error.message)) {
+                for (const message of error.message) {
+                    notification.add(message, "error");
+                }
+            } else {
+                notification.add(error.message, "error");
+            }
+        } else {
+            notification.add("Foglalás sikeresen létrehozva!", "success");
+        }
+    };
 
     return (
         <div className="container">
             <Calendar onChange={onChange} value={value} />
+            <button className="btn btn-primary mt-3" onClick={handleBooking}>
+                Foglalás
+            </button>
         </div>
     );
 }
